62bc415c85b992a1ca0b4c24e931dc289a706812
Michael Hermes <michaelehermes@gmail.com>
2016-10-16 21:28:12 -0500

    Initial commit. Probably need to rewrite everything.

---
+++ morgue_downloader.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# morgue_downloader.py 
+# Michael Hermes
+# v0.01 05/06/2012
+
+# First we want to read the HTML source for the page, making a list of all the
+# lines that contain what we're looking for: morgue text files. We strip all
+# of the characters that aren't part of the file name, and then download our 
+# list of files to the current directory. Python made this task pretty easy.
+
+import urllib
+from sys import exit
+
+no_connection = True
+while no_connection:
+    try:
+        url = raw_input('Please enter the url of your morgue directory: ')
+        response = urllib.urlopen(url)
+        no_connection = False
+    except:
+        print 'No connection, please check your url.'
+
+page_source = response.readlines()
+
+character_list = []
+for line in page_source:
+    if 'morgue' in line and '.txt' in line:
+        line = line.split('.txt">')[1]
+        line = line.split('</A>')[0]
+        character_list.append(line)
+
+if len(character_list) == 0:
+    print 'No valid character files in directory.'
+    exit_var = raw_input('Press any key to exit.')
+    exit()
+
+for character in character_list:
+    print 'Downloading ' + character
+    urllib.urlretrieve(url + character, character)
+    print character + ' downloaded successfully.'
+
+print 'Script completed successfully.'
